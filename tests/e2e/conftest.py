65c23dc3c1b84b319d46198cc207399f4ba206ab
Bruno Freitas <brunofreitasd11@gmail.com>
2026-05-19 18:38:08 -0300

    Correções selenium

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -7,6 +7,14 @@ from selenium.webdriver.chrome.options import Options
 
 from app import create_app
 
+options = Options()
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
+
+
 
 @pytest.fixture(scope="module")
 def live_server():
